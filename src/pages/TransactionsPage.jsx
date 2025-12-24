1ed3c43c977855b126aea657bb7e55bdb299c9a7
Madany Doumbia <cdoumbia@edane.org>
2025-12-24 16:22:30 +0000

    feat: ajout du FAB sur la page de transactions

--- src/pages/TransactionsPage.jsx
+++ src/pages/TransactionsPage.jsx
@@ -1,8 +1,10 @@
-export default function TransactionsPage() {
+import { FAB } from "@/components/ui/FAB"
 
+export default function TransactionsPage() {
     return (
         <>
             <h1>Transactions</h1>
+            <FAB />
         </>
     )
 }
